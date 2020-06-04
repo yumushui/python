33dec2c8512612176651e7cd1ca5c90d653e9cc2
yumushui <zhao113128@163.com>
2020-06-04 16:31:17 +0800

    Add a new file python_virtaul.py

---
+++ python_virtual.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+print('1. install virtualenv')
+print('pip install virtualenv')
+print('')
+
+print ('2. create a virtual env ')
+print ('mdkir project_dir')
+print ('vitualenv -p /usr/bin/python  venv27')
+print ('vitualenv -p /usr/local/bin/python3  venvr37')
+print ('')
+
+print ('3. active the virtual env')
+print ('source venv27/bin/activate')
+print ('')
+
+print ('4. deactive the virutal env')
+print ('deactivate')
+print ('')
+
