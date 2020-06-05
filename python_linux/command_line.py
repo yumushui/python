0caf9b158296e17356e9c354c68eccb4fd5dec60
yumushui <zhao113128@163.com>
2020-06-05 19:57:31 +0800

    Add a new command line.

---
+++ python_linux/command_line.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- conding:utf-8 -*-
+
+print('''
+The python used to make linux commade line
+1 python and command function
+    sys.argv  --get command paramter
+    sys.stdin  fileinput   --command input
+    sys.stdout  sys.stderr  --command print error info
+    getpass  gettuser    --command password get
+
+2 configpasre parse config file
+
+3 argparse  parse command parameter
+    argparse.ArgumentParse
+    mysql client command parameter
+    
+4 logging record logs
+    the use of logs
+    logging module
+
+5 open resourde python project
+    click  -- parse the command paramter
+    prompt_toolkit   -- make the interactive command line
+
+''')
